4126ca86423a27a5d2571d6c0bb5d18a9af19f8d
slevi123 <simofilevente@gmail.com>
2021-09-01 11:59:52 +0300

    o

--- docs/res/js/index.js
+++ docs/res/js/index.js
@@ -1,20 +1,29 @@
 // imports = ["les_canvas"]
 
+
 function createNotification(){
     var img = '/to-do-notifications/img/icon-128.png';
     var text = 'HEY! Your task "gg" is now overdue.';
     var notification = new Notification('To do list', { body: text, icon: img });
 }
 
-
-
-if (Notification.permission == "granted"){
-    createNotification();
-
-} else {
-    Notification.requestPermission().then(function(result) {
-        if (result="granted"){
-            createNotification();
-        }
-      });
+window.onload = function(){
+    const requested_dom = document.getElementById("requested")
+    const permission_dom = document.getElementById("permission")
+    const sent_dom = document.getElementById("sent")
+    let perm = Notification.permission;
+    console.log(permission_dom)
+    permission_dom.textContent = "permission: " + perm;
+    if (perm == "granted"){
+        createNotification();
+        sent_dom.textContent = "sent: yes";
+    } else {
+        Notification.requestPermission().then(function(result) {
+            requested_dom.textContent = "requested: " + result;
+            if (result="granted"){
+                createNotification();
+                sent_dom.textContent = "sent: yes";
+            }
+        });
+    }
 }
